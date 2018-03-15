2f0356967508753bb286f48a79d49b2dfdf03fb0
faisaltheparttimecoder <fai28683@gmail.com>
2018-03-15 11:52:19 +0000

    Added lifecycle page

---
+++ src/components/lifecycle/lifecycle.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="section">
+    <iframe :src="sourceURL">
+      Your browser doesn't support iframes
+    </iframe>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'lifecycle',
+    data: function() {
+      return {
+        sourceURL: 'https://d1fto35gcfffzn.cloudfront.net/support/PivotalLifecycleMatrix.pdf'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  iframe {
+    display: block;
+    border: none;
+    height: 95vh;
+    width: 97vw;
+  }
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,16 @@
 import security from '@/components/security/rssReader'
 import product from '@/components/products/products'
+import lifecycle from '@/components/lifecycle/lifecycle'
 
 export default [
   {
     path: '/security',
     component: security
-  },  {
+  },{
     path: '/products',
     component: product
+  },{
+    path: '/lifecycle',
+    component: lifecycle
   },
 ];
